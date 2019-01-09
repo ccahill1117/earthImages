027bae2518d84b3b80aaeb9eaa62f765346e3f97
christopher cahill and quinn miller <quinn@>
2019-01-09 10:41:43 -0800

    add test for game score

--- spec/game-spec.js
+++ spec/game-spec.js
@@ -1,9 +1,13 @@
 import { Game } from './../src/game.js';
-
-
-  describe ('Game', function() {
-  it('should display given player name', function() {
-    let string = new Game ("test");
-    expect(string.name).toEqual("test");
+const newGame = new Game ("test");
+// beforeEach(function() {
+// })
+describe ('Game', function() {
+  it('should contain and display given player name', function() {
+    // let string = new Game ("test");
+    expect(newGame.name).toEqual("test");
+  });
+  it('should contain and display given player score', function() {
+    expect(newGame.score).toEqual(0);
   })
 })
