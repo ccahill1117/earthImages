ef46e5c827eafc1b10cab08f4d4cb4d2165e6540
christopher cahill and quinn miller <quinn@>
2019-01-09 11:07:13 -0800

    add world js and failing test

--- spec/game-spec.js
+++ spec/game-spec.js
@@ -1,9 +1,10 @@
 import { Game } from './../src/game.js';
 import { Biker } from './../src/biker.js';
-const newGame = new Game ("test");
+import { World } from './../src/biker.js';
+const newGame = new Game("test");
 const newBiker = new Biker("test", 1);
-// beforeEach(function() {
-// })
+const newWorld = new World("test")
+
 describe ('Game', function() {
   it('should contain and display given game name', function() {
     // let string = new Game ("test");
@@ -20,5 +21,11 @@ describe ('Biker', function() {
   });
   it('should contain and display given player type', function(){
     expect(newBiker.type).toEqual(1);
-  })
-})
+  });
+});
+
+describe ('World', function() {
+  it('should create a bikeable world', function(){
+    expect(newWorld.size).toEqual([1,2,3,4,5]);
+  });
+});
---
+++ src/world.js
@@ -0,0 +1,4 @@
+export function World(name) {
+  this.name = name;
+  this.grid = [1,2,3,4,5];
+}
