fae7a5e60244fca09f2845e2ad994e0501353505
christopher cahill and quinn miller <christopher@>
2019-01-09 16:34:31 -0800

    final commit adds event listeners to arrow keys

--- src/main.js
+++ src/main.js
@@ -1,16 +1,51 @@
 import './styles.css';
-import { Game } from './game.js';
+// import { Game } from './game.js';
 import $ from 'jquery';
+
+const map = [
+  [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+  [1,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+  [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+  [1,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1],
+  [1,0,0,0,0,0,0,0,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,1],
+  [1,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,0,0,0,0,0,0,0,1],
+  [1,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,1],
+  [1,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+  [1,0,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1],
+  [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1],
+  [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+];
 
 
 $(document).ready(function(){
-  $("#inputForm").submit(function(event){
-    event.preventDefault();
 
-    let userString = $("#inputText").val();
-    let string = new Game(userString);
-    $(".resultDiv").empty();
-    $(".resultDiv").text(string.string);
-
+  $(document).keydown(function(e) {
+    switch (e.keyCode) {
+        case 37:
+            $(".resultDiv").append('left');
+            break;
+        case 38:
+            $(".resultDiv").append('up');
+            break;
+        case 39:
+            $(".resultDiv").append('right');
+            break;
+        case 40:
+            $(".resultDiv").append('down');
+            break;
+    }
   });
-});
+
+
+  map.forEach(function(line) {
+    line.forEach(function(element) {
+      if (element == 1) {
+        $("#grid").append("🌱")
+      } else if (element == 0) {
+        $("#grid").append("💩")
+      }
+    });
+    $("#grid").append("🌱").append("<br>");
+  })
+
+})
